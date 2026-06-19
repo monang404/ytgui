7f81ba2dee5bd9b4310e8d0dc0945a61ed1de70c
monang404 <bagassunandarh@gmail.com>
2026-06-19 14:01:14 +0700

    Adjust progress bar margin bottom to add vertical spacing below it

--- tui/components/progress_bar.py
+++ tui/components/progress_bar.py
@@ -10,7 +10,7 @@ class ClickableProgressBar(Static):
     ClickableProgressBar {
         height: 1;
         margin-top: 0;
-        margin-bottom: 0;
+        margin-bottom: 2;
     }
     """
     
