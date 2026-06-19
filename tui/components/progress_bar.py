3f820e78e7a11f002cc5b1aa8d8575cd6c9b28b3
monang404 <bagassunandarh@gmail.com>
2026-06-19 14:11:21 +0700

    menyesuaikan margin atas progress bar

--- tui/components/progress_bar.py
+++ tui/components/progress_bar.py
@@ -9,7 +9,7 @@ class ClickableProgressBar(Static):
     DEFAULT_CSS = """
     ClickableProgressBar {
         height: 1;
-        margin-top: 1;
+        margin-top: 0;
         margin-bottom: 1;
     }
     """
